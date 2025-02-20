1bb0e016e2c29beece7dc6ebcd62afd1a6e01ca0
Michael Shimeles <michaelwasihun96@gmail.com>
2025-02-20 10:12:45 -0500

    update gitignore

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -27,7 +27,7 @@ export default async function Navbar() {
                   Dashboard
                 </Button>
               </Link>
-              <UserProfile supabase={supabase} />
+              <UserProfile  />
             </>
           ) : (
             <>
